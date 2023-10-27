4e784a98aaa6396f839f21089a8dd99ede1e7a47
mofusand\Samenyan <yusrilnurhadi63@gmail.com>
2023-10-27 14:16:32 +0700

    Audio Murottal in Custom View AlertDialog

--- app/src/main/java/com/yus/quran/adapter/SurahAdapter.kt
+++ app/src/main/java/com/yus/quran/adapter/SurahAdapter.kt
@@ -10,6 +10,11 @@ import com.yus.quran.network.quran.QuranEdition
 class SurahAdapter : RecyclerView.Adapter<SurahAdapter.MyViewHolder>() {
     private val listAyah = ArrayList<AyahsItem>()
     private val listEdition = ArrayList<QuranEdition>()
+    private var onItemClickCallback: OnItemClickCallback? = null
+
+    fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback) {
+        this.onItemClickCallback = onItemClickCallback
+    }
 
     fun setData(dataAyah: List<AyahsItem>?, dataEdition: List<QuranEdition>?) {
         if (dataAyah == null || dataEdition == null) return
@@ -27,16 +32,22 @@ class SurahAdapter : RecyclerView.Adapter<SurahAdapter.MyViewHolder>() {
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val listAyah = listAyah[position]
-        val quranAudio = listEdition[1]
-        val quranTranslationIndo = listEdition[2]
+        val quranAudio = listEdition[1].listAyahs?.get(position)
+        val quranTranslationIndo = listEdition[2].listAyahs?.get(position)
 
         holder.binding.apply {
             itemNumberAyah.text = listAyah.numberInSurah.toString()
             itemAyah.text = listAyah.text
-            itemTranslation.text = quranTranslationIndo.listAyahs?.get(position)?.text
+            itemTranslation.text = quranTranslationIndo?.text
+            this.root.setOnClickListener {
+                quranAudio?.let { data -> onItemClickCallback?.onItemClicked(data) }
+            }
         }
     }
 
     override fun getItemCount() = listAyah.size
 
+    interface OnItemClickCallback {
+        fun onItemClicked(data: AyahsItem)
+    }
 }
--- app/src/main/java/com/yus/quran/presentation/quran/DetailSurahActivity.kt
+++ app/src/main/java/com/yus/quran/presentation/quran/DetailSurahActivity.kt
@@ -1,44 +1,106 @@
 package com.yus.quran.presentation.quran
 
-import androidx.appcompat.app.AppCompatActivity
+import android.media.AudioAttributes
+import android.media.MediaPlayer
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.yus.quran.R
 import com.yus.quran.adapter.SurahAdapter
 import com.yus.quran.databinding.ActivityDetailSurahBinding
+import com.yus.quran.databinding.CustomViewAlertdialogBinding
+import com.yus.quran.network.quran.AyahsItem
 import com.yus.quran.network.quran.SurahItem
 
 class DetailSurahActivity : AppCompatActivity() {
     private var _binding: ActivityDetailSurahBinding? = null
     private val binding get() = _binding as ActivityDetailSurahBinding
 
+    private var _surah: SurahItem? = null
+    private val surah get() = _surah as SurahItem
+
+    private var _mediaPlayer: MediaPlayer? = null
+    private val mediaPlayer get() = _mediaPlayer as MediaPlayer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityDetailSurahBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val data = intent.getParcelableExtra(EXTRA_DATA, SurahItem::class.java)
+        _surah = intent.getParcelableExtra(EXTRA_DATA, SurahItem::class.java)
 
         val quranViewModel = ViewModelProvider(this)[QuranViewModel::class.java]
-        data?.number?.let { quranViewModel.getListAyah(it) }
+        surah.number?.let { quranViewModel.getListAyah(it) }
 
-        binding.apply {
-            val revelationType = data?.revelationType
-            val numberOfAyahs = data?.numberOfAyahs
-            val resultOfAyah = "$revelationType - $numberOfAyahs Ayahs"
-            tvDetailAyah.text = resultOfAyah
-            tvDetailName.text = data?.name
-            tvDetailSurah.text = data?.englishName
-            tvDetailNameTranslation.text = data?.englishNameTranslation
-        }
+        initView()
+
+        val mAdapter = SurahAdapter()
+        mAdapter.setOnItemClickCallback(object : SurahAdapter.OnItemClickCallback {
+            override fun onItemClicked(data: AyahsItem) {
+                showCustomAlertDialog(data, surah)
+            }
+        })
 
         quranViewModel.listAyah.observe(this) {
             binding.rvSurah.apply {
-                val mAdapter = SurahAdapter()
                 mAdapter.setData(it.quranEdition?.get(0)?.listAyahs, it.quranEdition)
                 adapter = mAdapter
                 layoutManager = LinearLayoutManager(this@DetailSurahActivity)
             }
+        }
+    }
+
+    private fun initView() {
+        binding.apply {
+            val revelationType = surah.revelationType
+            val numberOfAyahs = surah.numberOfAyahs
+            val resultOfAyah = "$revelationType - $numberOfAyahs Ayahs"
+            tvDetailAyah.text = resultOfAyah
+            tvDetailName.text = surah.name
+            tvDetailSurah.text = surah.englishName
+            tvDetailNameTranslation.text = surah.englishNameTranslation
+        }
+    }
+
+    private fun showCustomAlertDialog(dataAudio: AyahsItem, surah: SurahItem) {
+        _mediaPlayer = MediaPlayer()
+        val builder = AlertDialog.Builder(this, R.style.CustomAlertDialog).create()
+        val view = CustomViewAlertdialogBinding.inflate(layoutInflater)
+        builder.setView(view.root)
+        view.apply {
+            tvSurah.text = surah.englishName
+            tvName.text = surah.name
+            val numberInSurah = dataAudio.numberInSurah
+            val resultNumberText = "Ayah $numberInSurah"
+            tvNumberAyah.text = resultNumberText
+        }
+        view.btnPlay.setOnClickListener {
+            it.isEnabled = false
+            view.btnPlay.text = getString(R.string.playing_audio)
+            mediaPlayer.setAudioAttributes(
+                AudioAttributes.Builder()
+                    .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                    .setUsage(AudioAttributes.USAGE_MEDIA)
+                    .build()
+            )
+            try {
+                mediaPlayer.setDataSource(dataAudio.audio)
+                mediaPlayer.prepare()
+                mediaPlayer.start()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+        view.btnCancel.setOnClickListener {
+            mediaPlayer.stop()
+            builder.dismiss()
+        }
+        builder.setCanceledOnTouchOutside(false)
+        builder.show()
+        mediaPlayer.setOnCompletionListener {
+            builder.dismiss()
         }
     }
 
